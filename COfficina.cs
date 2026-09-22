2d49aff295d1d3c08b791c01d2022ff979096c4e
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-22 18:58:02 +0200

    feat(gestionale): classe Officina base

---
+++ COfficina.cs
@@ -0,0 +1,39 @@
+using System;
+namespace Gestionale
+{
+
+    public interface IDescrizione
+    {
+        string Descrizione();
+    }
+    public class COfficina : IDescrizione
+    {
+        private string _nomeofficina;
+
+        public string nomeOfficina
+        {
+            get => _nomeofficina;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Il nome dell'officina non può essere vuoto");
+                }
+
+                _nomeofficina = value;
+            }
+        }
+
+        public COfficina() : this("SCONOSCIUTO") {}
+
+        public COfficina(string nomeofficina)
+        {
+            nomeOfficina = nomeofficina;
+        }
+
+        string Descrizione()
+        {
+            return $"Il nome dell'officina è: {nomeOfficina}";
+        }
+    }
+}
