f75d2a4a95eab2c040fb47ebf48a265c5953f89e
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-24 08:31:26 +0200

    feat(gestionale): Campi di CInterventi

---
+++ CInterventi.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Gestionale
+{
+    public class CInterventi
+    {
+        private CVeicolo[] _veicoli;
+        private CMeccanici[] _meccanici;
+
+        private string _data;
+        private string _descrizione;
+        private decimal _costoFinale;
+
+        
+    }
+}
