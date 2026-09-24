ea7f7c8c3c685c49937f7727395af28112ad2740
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-24 18:05:13 +0200

    setup main

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Gestionale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            COfficina officina = new COfficina("Officina Vicentina");
+        }
+    }
+}
