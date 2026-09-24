36619732bf117f3ed3ca6686d58363e15698e312
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-24 16:13:23 +0200

    feat(gestionale): campi e proprietà di CInterventi

--- CInterventi.cs
+++ CInterventi.cs
@@ -12,5 +12,45 @@ namespace Gestionale
         private bool _isInterventoChiuso;
 
 
+        public DateTime Data
+        {
+            get => _data;
+            private set
+            {
+                _data = value;
+            }
+        }
+
+        public string Descrizione
+        {
+            get => _descrizione;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("La descrizione non può essere nulla");
+                }
+                _descrizione = value;
+            }
+        }
+
+        public decimal costoFinale
+        {
+            get => _costoFinale;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Il costo finale non può essere negativo.");
+                }
+                _costoFinale = value;
+            }
+        }
+
+        public bool isInterventoChiuso
+        {
+            get => _isInterventoChiuso;
+            private set {}
+        }
     }
 }
